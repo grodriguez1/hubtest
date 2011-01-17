639dc8d877e2ca5a44b69f61338a334140dc49a6
grodriguez1 <g.rodrigue1@unibe.edu.do>
2011-01-17 12:35:37 -0800

    lets see if we create a conflict

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 int main(int argc, char **argv)
 {
 	var gab;//just added a new line from github online
-	char first[255], last[255];
+	char first[255], last[255];//lets see if we create a conflict from github.com
 
 	askname(first, last);
 
